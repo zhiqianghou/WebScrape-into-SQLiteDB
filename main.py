0cbdc877a8bdc5a90123fb234195ebd79ac7bc46
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-09-11 18:20:12 -0500

    Add send_email function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import selectorlib
+import smtplib, ssl
+import os
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
@@ -19,8 +21,17 @@ def extract(source):
 	return value
 
 
-def send_email():
-	print("Email was sent")
+def send_email(message):
+	host = "smtp.gmail.com"
+	port = 465
+	username = "houzh.py@gmail.com"
+	password = os.getenv("PASSWORD")
+	receiver = "houzh.py@gmail.com"
+	context = ssl.create_default_context()
+
+	with smtplib.SMTP_SSL(host, port, context=context) as server:
+		server.login(username, password)
+		server.sendmail(username, receiver, message)
 
 
 def store(extracted):
@@ -41,4 +52,4 @@ if __name__ == "__main__":
 	if extracted != "No upcoming tours":
 		if extracted not in contents:
 			store(extracted)
-			send_email()
+			send_email(message="Hey, new event was sent")
