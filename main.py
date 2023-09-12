4a8125d067df1b47b2c6bf6ed1defaa9431357f1
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-09-12 18:08:31 -0500

    Add vis_tempt.py, web_vis_tempt.py for visualization of the scrapped temperature

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import selectorlib
 import smtplib, ssl
 import os
 
+
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -45,6 +46,7 @@ def read(extracted):
 
 
 if __name__ == "__main__":
+	while True:
 	scraped = scrape(URL)
 	extracted = extract(scraped)
 	print(extracted)
@@ -53,3 +55,4 @@ if __name__ == "__main__":
 		if extracted not in contents:
 			store(extracted)
 			send_email(message="Hey, new event was sent")
+	time.sleep(2)
---
+++ web_vis_tempt.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+
+df = pd.read_csv("data_vis_tempt.txt")
+print(df.columns)
+
+figure = px.line(x=df['date'], y=df['temperature'],
+				 labels={'x':'Date', 'y':'Temperature(C)'})
+
+st.plotly_chart(figure)
